7522625bbb077fc102adee876d2b92c9785ed7a7
Natalia Lemańczyk <nataliaolga2@gmail.com>
2020-12-05 10:51:06 +0100

    changes in code

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
 const Hero = props => (
     <header className={styles.component}>
@@ -7,5 +8,9 @@ const Hero = props => (
         <img className={styles.image} src="https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
     </header>
 )
+
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+  };
 
 export default Hero;
